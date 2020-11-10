6d0ca28bbe9e8dcf46f3dfc57e57a712aa392673
JasonThian <100086673@students.swinburne.edu.my>
2020-11-10 16:05:48 +0800

    update compability to other device

--- app/src/main/java/com/example/visitorapp/MainActivity2.java
+++ app/src/main/java/com/example/visitorapp/MainActivity2.java
@@ -30,6 +30,7 @@ import java.util.Map;
 public class MainActivity2 extends AppCompatActivity {
     private String data = "", unit = "", contact = "", ic = "", username = "";
     private Date expire = new Date();
+    private Date currTime = new Date();
     private Map<String, Object> visitor = new HashMap<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,55 +57,58 @@ public class MainActivity2 extends AppCompatActivity {
             ic = datas[2];
             username = datas[3];
             expire.setTime(Long.parseLong(datas[4].trim()));
-            db.collection("visitor").whereEqualTo("username",username).whereEqualTo("expire_time",expire).limit(1).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                @Override
-                public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                    if(task.isSuccessful())
-                    {
-                        if(task.getResult().isEmpty())
-                        {
-                            visitor.put("unit",units);
-                            visitor.put("contact",contact);
-                            visitor.put("ic", ic);
-                            visitor.put("username",username);
-                            visitor.put("expire_time",expire);
-                            visitor.put("checkin_time",new Date());
-                            text.setVisibility(View.VISIBLE);
-                            carplateno.setVisibility(View.VISIBLE);
-                            submit.setVisibility(View.VISIBLE);
-                            submit.setOnClickListener(view -> {
-                                if(carplateno.getText().toString().matches(""))
-                                    visitor.put("carplate",null);
-                                else
-                                    visitor.put("carplate",carplateno.getText().toString());
-                                visitor.put("status","Check-In");
-                                db.collection("visitor").add(visitor).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<DocumentReference> task1) {
-                                        success();
-                                    }
-                                });
-                            });
-                        }
-                        else
-                        {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                if (document.get("status").toString().equals("Check-In")) {
-                                    Map<String,Object> updateData = new HashMap<>();
-                                    updateData.put("checkout_time", new Date());
-                                    updateData.put("status","Check-Out");
-                                    db.collection("visitor").document(document.getId()).update(updateData).addOnCompleteListener(new OnCompleteListener<Void>() {
+            if(currTime.before(expire)) {
+                db.collection("visitor").whereEqualTo("username", username).whereEqualTo("expire_time", expire).limit(1).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            if (task.getResult().isEmpty()) {
+                                visitor.put("unit", units);
+                                visitor.put("contact", contact);
+                                visitor.put("ic", ic);
+                                visitor.put("username", username);
+                                visitor.put("expire_time", expire);
+                                visitor.put("checkin_time", new Date());
+                                text.setVisibility(View.VISIBLE);
+                                carplateno.setVisibility(View.VISIBLE);
+                                submit.setVisibility(View.VISIBLE);
+                                submit.setOnClickListener(view -> {
+                                    if (carplateno.getText().toString().matches(""))
+                                        visitor.put("carplate", null);
+                                    else
+                                        visitor.put("carplate", carplateno.getText().toString());
+                                    visitor.put("status", "Check-In");
+                                    db.collection("visitor").add(visitor).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
                                         @Override
-                                        public void onComplete(@NonNull Task<Void> task) {
+                                        public void onComplete(@NonNull Task<DocumentReference> task1) {
                                             success();
                                         }
                                     });
+                                });
+                            } else {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    if (document.get("status").toString().equals("Check-In")) {
+                                        Map<String, Object> updateData = new HashMap<>();
+                                        updateData.put("checkout_time", new Date());
+                                        updateData.put("status", "Check-Out");
+                                        db.collection("visitor").document(document.getId()).update(updateData).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                success();
+                                            }
+                                        });
+                                    }
                                 }
                             }
                         }
                     }
-                }
-            });
+                });
+            }
+            else
+            {
+                mTextView.setVisibility(View.VISIBLE);
+                mTextView.setText("QR Code Expired");
+            }
         }
         else {
             mTextView.setVisibility(View.VISIBLE);
