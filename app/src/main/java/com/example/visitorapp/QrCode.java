d2f3b114686e6d86c97ae06ce30495cdaf15c5f5
JasonThian <100086673@students.swinburne.edu.my>
2020-11-10 11:31:23 +0800

    update compability to other device

---
+++ app/src/main/java/com/example/visitorapp/QrCode.java
@@ -0,0 +1,90 @@
+package com.example.visitorapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Size;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+
+public class QrCode extends AppCompatActivity {
+
+    private PreviewView previewView;
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private String qrCode;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_qr_code);
+        textView = findViewById(R.id.setText);
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        previewView = findViewById(R.id.activity_main_previewView);
+        // Enables Always-on
+        startCamera();
+    }
+
+    private void startCamera() {
+        cameraProviderFuture.addListener(() -> {
+            try {
+                Toast.makeText(getApplicationContext(),"Starting camera",Toast.LENGTH_SHORT).show();
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                QrCode.this.bindCameraPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                Toast.makeText(QrCode.this, "Error starting camera " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    private void bindCameraPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        previewView.setImplementationMode(PreviewView.ImplementationMode.PERFORMANCE);
+        Preview preview = new Preview.Builder()
+                .build();
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                ImageAnalysis imageAnalysis =
+                        new ImageAnalysis.Builder()
+                                .setTargetResolution(new Size(640, 720))
+                                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                                .build();
+                imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(QrCode.this), new QRCodeImageAnalyzer(new QRCodeFoundListener() {
+                    @Override
+                    public void onQRCodeFound(String _qrCode) {
+                        textView.setText(_qrCode);
+                        Toast.makeText(getApplicationContext(),"QR Code Found",Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(QrCode.this, MainActivity2.class);
+                        qrCode = _qrCode;
+                        /*intent.putExtra("data", qrCode);
+                        setResult(MainActivity.REQUEST_QRCODE, intent);
+                        finish();*/
+                    }
+                    @Override
+                    public void qrCodeNotFound() {
+                        textView.setText("QR Code Not Found");
+                    }
+                }));
+                Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)QrCode.this, cameraSelector, imageAnalysis, preview);
+            }
+        });
+    }
+}
